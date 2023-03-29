15432ac5ad99fd9e137bdfa2928e502017ddcb88
AlbertHuang <r.albert.huang@gmail.com>
2023-03-29 14:44:52 +0800

    fix: block parse

--- chains/eth/watcher.go
+++ chains/eth/watcher.go
@@ -261,10 +261,12 @@ func (w *Watcher) processBlock(block *ethtypes.Block) *txReceiptResponse {
 		receipts:    make([]*ethtypes.Receipt, 0),
 	}
 
-	// if !block.Header().Bloom.Test([]byte(w.vault)) {
-	// 	log.Info("No vault address in bloom filter, skipping block", w.vault)
-	// 	return ret
-	// }
+	vaultAddress := common.HexToAddress(w.vault)
+
+	if !block.Header().Bloom.Test(vaultAddress.Bytes()) {
+		log.Info("No vault address in bloom filter, skipping block", w.vault)
+		return ret
+	}
 
 	txs := block.Transactions()
 
